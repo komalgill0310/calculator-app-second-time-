737f6474a7d9ccd89d3637d2c58046e3d1f8fddd
Komal Gill <komalgill0310@gmail.com>
2022-02-13 07:15:49 -0500

    still not able to add single digit numbers

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// const inputValue = document.querySelector("#result");
+const inputValue = document.querySelector("#result");
+
+showOperands();
+function showOperands(){
 const operands = document.querySelectorAll(".operand-container");
 operands.forEach(function (operand) {
   operand.addEventListener("click", (e) => {
@@ -6,6 +9,7 @@ operands.forEach(function (operand) {
     inputValue.value = e.target.value;
   });
 });
+}
 
 const operators = document.querySelectorAll(".operator-container");
 operators.forEach(function (operator) {
@@ -17,16 +21,28 @@ operators.forEach(function (operator) {
 
 document.querySelector("#equal-btn").addEventListener("click", (e) => {
   e.preventDefault();
-  // add();
+  add();
 });
 
-
+function add() {
+  let x;
+  let y;
+  if (!inputValue.value) {
+     x = showOperands();
+     console.log("firstOperand: ", x);
+  }   //user enters 2 => x = 2
+  else {
+     y = showOperands(); //user enters 1 => y = 1
+     console.log("secondOperand: ", y);
+  }
+  console.log(parseInt(x)+parseInt(y)); //=> 3
+}
 
 // experiment#1: 
 // 1. get the input element and store that element in a global variable
 // const inputValue = document.querySelector("#result");
 // 2. define a funtion showOperands and store the logic of displaying operand in input box when click on number button
-  // 2.1 return input.value from showOperands function
+// 2.1 return input.value from showOperands function
 // function showoperands() {
 //   operands.forEach(function (operand) {
 //     operand.addEventListener("click", (e) => {
@@ -37,12 +53,16 @@ document.querySelector("#equal-btn").addEventListener("click", (e) => {
 //   return inputValue.value;
 // }
 //4. create function add
-  //4.1 call showOperands function and perform the add operation and display that value in console.log
+//4.1 call showOperands function and perform the add operation and display that value in console.log
 //ERROR: Based on this logic: the inputValue gets updated, when user click on the operand button the second time. 
 //Expectation: e.g.: function add(){
-//  x = showoperands(); //user enters 2 => x = 2
-//  y = showoperands(); //user enters 1 => y = 1
-//   console.log(x); // 2
+// if (input.value === "") {
+  //  x = showoperands();
+// }   //user enters 2 => x = 2
+//  else{
+// y = showoperands(); //user enters 1 => y = 1
+//   }
+  //   console.log(x); // 2
 //   console.log(y); //1
   // console.log(parseInt(x)+parseInt(y)); => 3
 // }
