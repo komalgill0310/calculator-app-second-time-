a29b691f47c405b556f170fed94cc103274f7ffa
Komal Gill <komalgill0310@gmail.com>
2022-02-14 13:35:58 -0500

    some css modifications

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ const operators = document.querySelectorAll(".operator");
 operators.forEach((operatorValue) => {
   operatorValue.addEventListener("click", (e) => {
     e.preventDefault();
-    if(firstOperand){
+    if (firstOperand) {
       operator = e.target.textContent;
       console.log("operator: ", operator);
-      result.value = e.target.textContent;
-    }   
+      result.textContent = e.target.textContent;
+    }
   });
 });
 
@@ -19,16 +19,48 @@ const numbers = document.querySelectorAll(".number");
 numbers.forEach((number) => {
   number.addEventListener("click", (e) => {
     e.preventDefault();
-    if(!operator){
-      firstOperand = e.target.textContent;
+    if (!operator) {
+      firstOperand += e.target.textContent;
       console.log("firstOperand: ", firstOperand);
       result.textContent = firstOperand;
     }
-    if(firstOperand && operator){
-      secondOperand = e.target.textContent;
+    if (firstOperand && operator) {
+      secondOperand += e.target.textContent;
       console.log("secondOperand: ", secondOperand);
       result.textContent = secondOperand;
     }
   });
 });
 
+document.querySelector("#equal-btn").addEventListener("click", (e) => {
+  e.preventDefault();
+  calculate();
+})
+
+function calculate() {
+  let firstNumber = parseFloat(firstOperand);
+  let secondNumber = parseFloat(secondOperand);
+  switch (operator) {
+    case "+":
+      result.textContent = firstNumber + secondNumber;
+      console.log(result.textContent);
+      break;
+    case "-":
+      result.textContent = firstNumber - secondNumber;
+      console.log(result.textContent);
+      break;
+    case "X":
+      result.textContent = firstNumber * secondNumber;
+      console.log(result.textContent);
+      break;
+    default:
+      result.textContent = firstNumber / secondNumber;
+      console.log(result.textContent);
+      break;
+  }
+}
+
+// document.querySelector("#reset-btn").addEventListener("click", (e) => {
+//   e.preventDefault();
+//   result.textContent = "0";
+// });
